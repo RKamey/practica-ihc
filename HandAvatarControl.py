302bfc5ac6012c1b678c6c9b16b2b05b4d132cfa
RKamey <ralonso@ucol.mx>
2023-09-23 23:20:15 -0600

    ya jala

--- HandAvatarControl.py
+++ HandAvatarControl.py
@@ -3,8 +3,9 @@ import mediapipe as mp
 import numpy as np
 import pygame as pg
 
-mp_hands = mp.solutions.hands
 mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_hands = mp.solutions.hands
 
 # Inicializa la cámara
 cap = cv2.VideoCapture(0)
@@ -19,19 +20,20 @@ frame = np.zeros((height, width, 3), dtype=np.uint8)
 # Definir las coordenadas del rectángulo (mousepad)
 mousepad_x, mousepad_y, mousepad_width, mousepad_height = 100, 100, 440, 280
 
+# Configuración de Pygame
+pg.init()
+pg_width, pg_height = 600, 600
+pg_screen = pg.display.set_mode((pg_width, pg_height))
+pg.display.set_caption("Control de Avatar con Mano")
+pg.display.set_icon(pg.image.load("./images/gestual.jpg"))
+pg_clock = pg.time.Clock()
+
 # Definir el tamaño de la cuadrícula del juego
 grid_size = 3
-cell_size = mousepad_width // grid_size
+cell_size = pg_width // grid_size
 
 # Inicializar la posición del avatar en el centro de la cuadrícula
 avatar_x, avatar_y = grid_size // 2, grid_size // 2
-
-# Configuración de Pygame
-pg.init()
-pg_width, pg_height = 640, 480
-pg_screen = pg.display.set_mode((pg_width, pg_height))
-pg.display.set_caption("Control de Avatar con Mano")
-pg_clock = pg.time.Clock()
 
 while True:
     ret, image = cap.read()
@@ -41,11 +43,11 @@ while True:
     # Voltea la imagen horizontalmente para que sea como un espejo
     image = cv2.flip(image, 1)
 
+    # Dibuja el rectángulo (mousepad) en la ventana de OpenCV
+    cv2.rectangle(image, (mousepad_x, mousepad_y), (mousepad_x + mousepad_width, mousepad_y + mousepad_height), (0, 255, 0), 2)
+
     # Procesa la imagen para detectar las manos
     results = hands.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-
-    # Dibuja el rectángulo (mousepad)
-    cv2.rectangle(image, (mousepad_x, mousepad_y), (mousepad_x + mousepad_width, mousepad_y + mousepad_height), (0, 255, 0), 2)
 
     if results.multi_hand_landmarks:
         for hand_landmarks in results.multi_hand_landmarks:
@@ -55,33 +57,33 @@ while True:
             index_finger = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP]
 
             # Mapea la posición del dedo índice en el "mousepad" a la cuadrícula del juego
-            avatar_x = int((index_finger.x * mousepad_width) / width)
-            avatar_y = int((index_finger.y * mousepad_height) / height)
+            avatar_x = int((index_finger.x * grid_size))
+            avatar_y = int((index_finger.y * grid_size))
 
-    # Dibuja el avatar en la cuadrícula
+    # Dibuja el avatar en la cuadrícula de Pygame
     avatar_x = min(max(0, avatar_x), grid_size - 1)
     avatar_y = min(max(0, avatar_y), grid_size - 1)
 
     # Limpiar la pantalla de Pygame
     pg_screen.fill((255, 255, 255))
 
-    # Dibuja la cuadrícula del juego
+    # Dibuja la cuadrícula del juego en Pygame
     for x in range(grid_size):
         for y in range(grid_size):
-            cell_x = mousepad_x + x * cell_size
-            cell_y = mousepad_y + y * cell_size
+            cell_x = x * cell_size
+            cell_y = y * cell_size
             pg.draw.rect(pg_screen, (0, 0, 0), (cell_x, cell_y, cell_size, cell_size), 1)
 
-    # Dibuja el avatar en la cuadrícula
-    cell_x = mousepad_x + avatar_x * cell_size
-    cell_y = mousepad_y + avatar_y * cell_size
+    # Dibuja el avatar en la cuadrícula de Pygame
+    cell_x = avatar_x * cell_size
+    cell_y = avatar_y * cell_size
     avatar_color = (255, 0, 0)
     pg.draw.circle(pg_screen, avatar_color, (cell_x + cell_size // 2, cell_y + cell_size // 2), cell_size // 2)
 
-    # Muestra la imagen de la cámara
+    # Muestra la ventana de OpenCV
     cv2.imshow('Hand Tracking', image)
 
-    # Actualiza la pantalla de Pygame
+    # Muestra la ventana de Pygame
     pg.display.flip()
 
     for event in pg.event.get():
@@ -90,8 +92,6 @@ while True:
             cv2.destroyAllWindows()
             pg.quit()
             exit(0)
-
-    pg_clock.tick(60)
 
     if cv2.waitKey(1) == ord('q'):
         break
